9ef5841926898c4f54e12b2bf20ad1fc97f495dd
smgvasya <deepxp@yandex.ru>
2022-08-22 02:00:11 +0300

    Fix and open and firstcard_like_delet

--- pages/script.js
+++ pages/script.js
@@ -15,28 +15,41 @@ const popupProfileSubmit = container.querySelector('#popup-submit-profile');
 const popupMestoSubmit = container.querySelector('#popup-submitmesto');
 const popups = container.querySelector('.popup');
 const formProfile = container.querySelector('#form-profile');
+const formMesto = container.querySelector('#edit-mesto');
+const elements = container.querySelector('.element');
+const elementLike = container.querySelector('.element__button-like');
+const deleteCard = container.querySelector('.element__button-trash');
+const mestoName = container.querySelector('#edit-mesto-title');
+const mestoLink = container.querySelector('#edit-mesto-link');
+const popupImg = container.querySelector('.popup__img');
+const popupPhotoTitle = container.querySelector('.popup__photo-title');
 
+// открытие модального окна
 function popupOpened(popup)  {
     popup.classList.add('popup_opened')
 };
 
+// закрытие модального окна
 function popupClose(popup)  {
     popup.classList.remove('popup_opened');
 };
 
+
+// при сохранении нового места мелькают все формы
+function formSubmitProfile (evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileAbout.textContent = aboutInput.value;
+    popupClose(popupProfile); 
+}
+formProfile.addEventListener('submit', formSubmitProfile);
+
+
 addButtonProfile.addEventListener('click', function(){
-    popupOpened(popupProfile);
     nameInput.value = profileName.textContent;
     aboutInput.value = profileAbout.textContent;
+    popupOpened(popupProfile);
 });
-
-//не получается изменить инфу в профидле
-// function formSubmitHandler (evt) {
-//     evt.preventDefault();
-//     profileName.textContent = nameInput.value;
-//     profileAbout.textContent = textContent.value;  
-// }
-// formProfile.addEventListener('submit', formSubmitHandler);
 
 
 closePopupProfile.addEventListener('click', function(){
@@ -48,52 +61,92 @@ addButtonPhoto.addEventListener('click', function(){
     popupOpened(popupMesto);
 });
 
+
 closePopupMesto.addEventListener('click', function(){
 popupClose(popupMesto);
 });
 
 
-
-// при сохранинии выскакивают все формы
-
-
-
-
+//работает только для первой карточки
+elementLike.addEventListener('click', function (evt) {
+    elementLike.classList.toggle('element__button-like_active');
+ });
 
 
+//работает только для первой карточки
+deleteCard.addEventListener('click', (evt) => {
+    evt.target.closest(".element").remove();
+});
 
 
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+    ];
 
 
+function addCard(name, link) {
+const cardTemplate = document.querySelector('#card-template').content;
+const cardElements = cardTemplate.querySelector('.element').cloneNode(true);
+
+const photo = cardElements.querySelector('element__photo');
+const title = cardElements.querySelector('element__title');
 
 
+photo.src = link;
+title.textContent = name;
+
+elementLike.addEventListener('click', function (evt) {
+    elementLike.classList.toggle('element__button-like_active');
+ });
+
+deleteCard.addEventListener('click', (evt) => {
+    evt.target.closest(".element").remove();
+});
+
+photo.addEventListener("click", (event) => {
+    popupOpened(popup-photo);
+    popupImg.src = link;
+    popupPhotoTitle.textContent = name;
+  });
+  return cardElements;
+  elementsContainer.prepend(cardElements);
+};
 
 
+// function formSubmitMesto (evt) {
+//     evt.preventDefault();
+//     profileName.textContent = mestoName.value;
+//     profileAbout.textContent = mestolink.value;
+//     popupClose(popupProfile); 
+// }
+// formMesto.addEventListener('submit', formSubmitMesto);
 
 
-// const initialCards = [
-//     {
-//       name: 'Архыз',
-//       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-//     },
-//     {
-//       name: 'Челябинская область',
-//       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-//     },
-//     {
-//       name: 'Иваново',
-//       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-//     },
-//     {
-//       name: 'Камчатка',
-//       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-//     },
-//     {
-//       name: 'Холмогорский район',
-//       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-//     },
-//     {
-//       name: 'Байкал',
-//       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-//     }
-//     ];
+// addButtonProfile.addEventListener('click', function(){
+//     nameInput.value = profileName.textContent;
+//     aboutInput.value = profileAbout.textContent;
+//     popupOpened(popupProfile);
+// });
