0734a73039b73a3103bf82b6be0a9efb28cc3ec1
smgvasya <deepxp@yandex.ru>
2022-08-22 23:04:48 +0300

    like/delete/fixgrid

--- pages/script.js
+++ pages/script.js
@@ -17,12 +17,17 @@ const popups = container.querySelector('.popup');
 const formProfile = container.querySelector('#form-profile');
 const formMesto = container.querySelector('#edit-mesto');
 const elements = container.querySelector('.element');
-const elementLike = container.querySelector('.element__button-like');
-const deleteCard = container.querySelector('.element__button-trash');
+const elementLikes = container.querySelectorAll('.element__button-like');
+const deleteCards = container.querySelectorAll('.element__button-trash');
 const mestoName = container.querySelector('#edit-mesto-title');
 const mestoLink = container.querySelector('#edit-mesto-link');
 const popupImg = container.querySelector('.popup__img');
 const popupPhotoTitle = container.querySelector('.popup__photo-title');
+
+
+
+
+
 
 // открытие модального окна
 function popupOpened(popup)  {
@@ -34,7 +39,6 @@ function popupClose(popup)  {
     popup.classList.remove('popup_opened');
 };
 
-
 // при сохранении нового места мелькают все формы
 function formSubmitProfile (evt) {
     evt.preventDefault();
@@ -44,13 +48,11 @@ function formSubmitProfile (evt) {
 }
 formProfile.addEventListener('submit', formSubmitProfile);
 
-
 addButtonProfile.addEventListener('click', function(){
     nameInput.value = profileName.textContent;
     aboutInput.value = profileAbout.textContent;
     popupOpened(popupProfile);
 });
-
 
 closePopupProfile.addEventListener('click', function(){
 popupClose(popupProfile);
@@ -67,44 +69,27 @@ popupClose(popupMesto);
 });
 
 
-//работает только для первой карточки
-elementLike.addEventListener('click', function (evt) {
-    elementLike.classList.toggle('element__button-like_active');
- });
-
-
-//работает только для первой карточки
-deleteCard.addEventListener('click', (evt) => {
-    evt.target.closest(".element").remove();
+//лайк карточек
+elementLikes.forEach(likeBtn => {
+likeBtn.addEventListener('click', (evt) => {
+  evt.target.classList.toggle('element__button-like_active');
+});
 });
 
 
-const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-    ];
+//удаление карточек
+deleteCards.forEach(delCard => {
+  delCard.addEventListener('click', (evt) => {
+    evt.target.closest(".element").remove();
+});
+});
+
+
+//   function loadCards() {
+//     initialCards.map(card => addCard(card.name, card.link));
+  
+//   }
+//   loadCards();
 
 
 function addCard(name, link) {
@@ -134,6 +119,8 @@ photo.addEventListener("click", (event) => {
   return cardElements;
   elementsContainer.prepend(cardElements);
 };
+
+
 
 
 // function formSubmitMesto (evt) {
