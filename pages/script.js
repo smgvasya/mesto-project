f2bad75f0b2a0b955c70635b8843a0e89c255bf4
smgvasya <deepxp@yandex.ru>
2022-08-25 02:41:58 +0300

    notAtAll

--- pages/script.js
+++ pages/script.js
@@ -1,57 +1,132 @@
 const container = document.querySelector('.page');
+
 const addButtonProfile = container.querySelector('.profile__button');
 const addButtonPhoto = container.querySelector('.profile__button-photo');
-const elementsContainer = container.querySelector('.elements__container');
+
+const popups = container.querySelector('.popup');
+
+const popupProfile = container.querySelector('#popup-profile');
+const closePopupProfile = document.querySelector('#popup-profile-close');
+const popupProfileSubmit = container.querySelector('#popup-submit-profile');
 const profileName = container.querySelector('.profile__name');
 const profileAbout = container.querySelector('.profile__about');
 const nameInput = document.querySelector('#edit-profile-name');
 const aboutInput = document.querySelector('#edit-profile-about');
-const closePopupProfile = document.querySelector('#popup-profile-close');
-const closePopupMesto = document.querySelector('#popup-mesto-close');
-const popupProfile = container.querySelector('#popup-profile');
-const popupMesto = container.querySelector('#popup-mesto');
-const popupPhoto = container.querySelector('#popup-photo');
-const popupProfileSubmit = container.querySelector('#popup-submit-profile');
-const popupMestoSubmit = container.querySelector('#popup-submitmesto');
-const popups = container.querySelector('.popup');
 const formProfile = container.querySelector('#form-profile');
+
+
+const popupMesto = container.querySelector('#popup-mesto');
+const closePopupMesto = document.querySelector('#popup-mesto-close');
+const popupMestoSubmit = container.querySelector('#popup-submit-mesto');
 const formMesto = container.querySelector('#edit-mesto');
-const elements = container.querySelector('.element');
-const elementLikes = container.querySelectorAll('.element__button-like');
-const deleteCards = container.querySelectorAll('.element__button-trash');
 const mestoName = container.querySelector('#edit-mesto-title');
 const mestoLink = container.querySelector('#edit-mesto-link');
-const popupImg = container.querySelector('.popup__img');
-const popupPhotoTitle = container.querySelector('.popup__photo-title');
 
 
+const popupPhoto = container.querySelector('#popup-photo');
+const closePopupPhoto = popupPhoto.querySelector('#popup-photo-close');
+const popupImg = popupPhoto.querySelector('.popup__img');
+const popupPhotoTitle = popupPhoto.querySelector('.popup__photo-title');
+
+const elementsContainer = container.querySelector('.elements__container');
+const elements = container.querySelector('.element');
+const clickPhotoCard = container.querySelectorAll('.element__photo');
+const elementLikes = container.querySelectorAll('.element__button-like');
+const deleteCards = container.querySelectorAll('.element__button-trash');
+const elementTitle = container.querySelectorAll('.element__title');
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+  ];
 
 
+  // function addCard (element) {
+  //   const cardTemplate = document.querySelector('#card-template').content;
+  //   const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+  //   const photo = cardElement.querySelector('element__photo');
+  //   const title = cardElement.querySelector('element__title');
+    
+  //   photo.src = element.link;
+  //   photo.alt = element.name;
+  //   title.textContent = element.name;
+  
+  //   photo.addEventListener('click', () => openLargePhoto(element));
+  //   return cardElement;
+  // };
 
 
-// открытие модального окна
+//лайк карточек
+elementLikes.forEach(likeBtn => {
+  likeBtn.addEventListener('click', (evt) => {
+    evt.target.classList.toggle('element__button-like_active');
+  });
+  });
+  
+  //удаление карточек
+  deleteCards.forEach(delCard => {
+    delCard.addEventListener('click', (evt) => {
+      evt.target.closest(".element").remove();
+  });
+  });
+
+  clickPhotoCard.addEventListener('click', function(){
+    // popupImg.src = clickPhotoCard.link;
+    // popupImg.alt = clickPhotoCard.name;
+    // popupPhotoTitle.textContent = elementTitle.name;
+    popupOpened(popupPhoto);
+  });
+  
+  closePopupPhoto.addEventListener('click', function(){
+  popupClose(popupPhoto);
+  });
+  
+
+  // открытие модального окна
 function popupOpened(popup)  {
-    popup.classList.add('popup_opened')
+  popup.classList.add('popup_opened')
 };
 
 // закрытие модального окна
 function popupClose(popup)  {
-    popup.classList.remove('popup_opened');
+  popup.classList.remove('popup_opened');
 };
 
 // при сохранении нового места мелькают все формы
 function formSubmitProfile (evt) {
-    evt.preventDefault();
-    profileName.textContent = nameInput.value;
-    profileAbout.textContent = aboutInput.value;
-    popupClose(popupProfile); 
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileAbout.textContent = aboutInput.value;
+  popupClose(popupProfile); 
 }
 formProfile.addEventListener('submit', formSubmitProfile);
 
 addButtonProfile.addEventListener('click', function(){
-    nameInput.value = profileName.textContent;
-    aboutInput.value = profileAbout.textContent;
-    popupOpened(popupProfile);
+  nameInput.value = profileName.textContent;
+  aboutInput.value = profileAbout.textContent;
+  popupOpened(popupProfile);
 });
 
 closePopupProfile.addEventListener('click', function(){
@@ -60,7 +135,8 @@ popupClose(popupProfile);
 
 
 addButtonPhoto.addEventListener('click', function(){
-    popupOpened(popupMesto);
+  // popupMesto.reset();
+  popupOpened(popupMesto);
 });
 
 
@@ -69,71 +145,23 @@ popupClose(popupMesto);
 });
 
 
-//лайк карточек
-elementLikes.forEach(likeBtn => {
-likeBtn.addEventListener('click', (evt) => {
-  evt.target.classList.toggle('element__button-like_active');
-});
-});
-
-
-//удаление карточек
-deleteCards.forEach(delCard => {
-  delCard.addEventListener('click', (evt) => {
-    evt.target.closest(".element").remove();
-});
-});
-
-
-//   function loadCards() {
-//     initialCards.map(card => addCard(card.name, card.link));
-  
-//   }
-//   loadCards();
-
-
-function addCard(name, link) {
-const cardTemplate = document.querySelector('#card-template').content;
-const cardElements = cardTemplate.querySelector('.element').cloneNode(true);
-
-const photo = cardElements.querySelector('element__photo');
-const title = cardElements.querySelector('element__title');
-
-
-photo.src = link;
-title.textContent = name;
-
-elementLike.addEventListener('click', function (evt) {
-    elementLike.classList.toggle('element__button-like_active');
- });
-
-deleteCard.addEventListener('click', (evt) => {
-    evt.target.closest(".element").remove();
-});
-
-photo.addEventListener("click", (event) => {
-    popupOpened(popup-photo);
-    popupImg.src = link;
-    popupPhotoTitle.textContent = name;
+closePopupPhoto.addEventListener('click', function(){
+  popupClose(popupPhoto);
   });
-  return cardElements;
-  elementsContainer.prepend(cardElements);
-};
+
+  
+
+ function formSubmitMesto (evt) {
+  evt.preventDefault();
+  const element = {
+    name: mestoName.value,
+    link: mestoLink.value,
+  };
+  elementsContainer.prepend(addCard(element));
+  popupClose(popupMesto);
+}
+popupMestoSubmit.addEventListener('submit', formSubmitMesto);
 
 
 
 
-// function formSubmitMesto (evt) {
-//     evt.preventDefault();
-//     profileName.textContent = mestoName.value;
-//     profileAbout.textContent = mestolink.value;
-//     popupClose(popupProfile); 
-// }
-// formMesto.addEventListener('submit', formSubmitMesto);
-
-
-// addButtonProfile.addEventListener('click', function(){
-//     nameInput.value = profileName.textContent;
-//     aboutInput.value = profileAbout.textContent;
-//     popupOpened(popupProfile);
-// });
