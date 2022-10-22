ce9a753fdb560e7b17c478d3964c2d2c3beeca6d
smgvasya <deepxp@yandex.ru>
2022-10-22 03:29:02 +0300

    add_error

--- script/script.js
+++ script/script.js
@@ -24,15 +24,17 @@ const mestoLink = document.querySelector('#edit-mesto-link');
 
 const closeButtons = document.querySelectorAll('.popup__close');
 
-const popup = document.querySelectorAll('.popup');
+
 //Функция открытие модальных окон
 function openPopup(popup)  {
   popup.classList.add('popup_opened');
+  document.addEventListener("keydown", closePopupEsc);
 }
 
 //Функция закрытия модальных окон
 function closePopup(popup)  {
   popup.classList.remove('popup_opened');
+  document.removeEventListener("keydown", closePopupEsc);
 }
 
 //Функция закрытия мадальных окон по esc
@@ -73,10 +75,7 @@ addButtonProfile.addEventListener('click', function (){
 closeButtons.forEach((button) => {
   const popup = button.closest('.popup');
   button.addEventListener('click', () => closePopup(popup));
-  closePopupOverlay (popupProfile);
-  closePopupOverlay (popupMesto);
-  closePopupOverlay (popupPhoto);
-  document.addEventListener('keydown', closePopupEsc);
+  closePopupOverlay (popup);
 });
 
 //Открытие окна добавление карточки
@@ -145,4 +144,67 @@ function SubmitFormMesto(evt) {
 }
 formMesto.addEventListener ('submit', SubmitFormMesto);
 
+//Валидация
 
+
+const formElement = document.querySelector('.form');
+const formInput = formElement.querySelector('.form__profile-input');
+const formError = formElement.querySelector(`.${formInput.id}-error`);
+
+// function showInputError = (formElement, inputElement, errorMessage) => {
+//   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+//   inputElement.classList.add('form__profile-error');
+//   errorElement.textContent = errorMessage;
+//   errorElement.classList.add('form__input-error_active');
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const setEventListeners = (formElement) => {
+//   const inputList = Array.from(formElement.querySelectorAll('.form__profile-input'));
+//   inputList.forEach((inputElement) => {
+//     inputElement.addEventListener('input', function () {
+//       checkInputValidity(formElement, inputElement);
+//     });
+//   });
+// };
+
+// function enableValidation () {
+//   const formList = Array.from(document.querySelectorAll('.form'));
+//   formList.forEach((formElement) => {
+//   formElement.addEventListener('submit', (evt) => {
+//     evt.preventDefault();
+//   });
+
+//     setEventListeners(formElement);
+// });
+
+// };
+
+// enableValidation();
+
+
+// const isValid = (formElement, inputElement) => {
+//   if (inputElement.validity.patternMismatch) {
+//   inputElement.setCustomValidity(inputElement.dataset.errorMessage);
+// } else {
+//   inputElement.setCustomValidity("");
+// }
+
+// if (!inputElement.validity.valid) {
+//   showInputError(formElement, inputElement, inputElement.validationMessage);
+// } else {
+//   hideInputError(formElement, inputElement);
+// }
+// };
